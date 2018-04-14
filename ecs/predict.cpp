c0437e3bda32d7f09c09566e93af93b34c5eaa1f
xingyang-liu <billyliu@sjtu.edu.cn>
2018-04-15 03:03:34 +0800

    Second Edition

--- ecs/predict.cpp
+++ ecs/predict.cpp
@@ -10,81 +10,81 @@ using namespace std;
 //��Ҫ��ɵĹ��������
 void predict_server(char * info[MAX_INFO_NUM], char * data[MAX_DATA_NUM], int data_num, char * filename)
 {
-	//��ȡ�����ļ�
-	int CPU, MEM, DISK = 0;
-	sscanf(info[0], "%d %d %d", &CPU, &MEM, &DISK);
-	int numFlavorType = 0;
-	sscanf(info[2], "%d", &numFlavorType);
+    //��ȡ�����ļ�
+    int CPU, MEM, DISK = 0;
+    sscanf(info[0], "%d %d %d", &CPU, &MEM, &DISK);
+    int numFlavorType = 0;
+    sscanf(info[2], "%d", &numFlavorType);
     char tmpFlavorName[512] = "";
-	char ** tflavorName = new char*[numFlavorType];
-	for (int i=0;i<numFlavorType;i++){
-	    tflavorName[i] = new char[10];
-	}
+    char ** tflavorName = new char*[numFlavorType];
+    for (int i=0;i<numFlavorType;i++){
+        tflavorName[i] = new char[10];
+    }
     vector<char *> V_name;
     vector<int> V_CPU;
     vector<int> V_MEM;
-	int flavorCPU = 0;
-	int flavorMEM = 0;
-	for (int i = 3; i < numFlavorType+3; i++) {
-		sscanf(info[i], "%s %d %d", tmpFlavorName, &flavorCPU, &flavorMEM);
-		strcpy(tflavorName[i-3], tmpFlavorName);
+    int flavorCPU = 0;
+    int flavorMEM = 0;
+    for (int i = 3; i < numFlavorType+3; i++) {
+        sscanf(info[i], "%s %d %d", tmpFlavorName, &flavorCPU, &flavorMEM);
+        strcpy(tflavorName[i-3], tmpFlavorName);
         V_CPU.push_back(flavorCPU);
         V_MEM.push_back(flavorMEM/1024);
         V_name.push_back(tflavorName[i-3]);
 //        cout << tmpFlavorName[i-3]<<endl;
 //        cout <<info[i];
-	}
-	char optType[5] = "";
-	sscanf(info[numFlavorType + 4], "%s", optType);
-	bool memOrCPU;
-	if (!strcmp(optType, "CPU"))
-	    memOrCPU = true;
-	else
-	    memOrCPU = false;
-	struct tm timeStamp1, timeStamp2;
-	int year, month, day, hour, minute, second;
-	sscanf(info[numFlavorType + 6], "%d-%d-%d %d:%d:%d", &year, &month, &day, &hour, &minute, &second);
-	timeStamp1.tm_year = year - 1900;                 // �꣬����tm�ṹ��洢���Ǵ�1900�꿪ʼ��ɦ�䣬����tm_yearΪint��ɦ������ȥ1900��
-	timeStamp1.tm_mon = month - 1;                    // �£�����tm�ṹ����·ݴ洢��ΧΪ0-11������tm_monΪint��ɦ������ȥ1��
-	timeStamp1.tm_mday = day;                         // �ա�
-	timeStamp1.tm_hour = hour;                        // ɦ��
-	timeStamp1.tm_min = minute;                       // �֡�
-	timeStamp1.tm_sec = second;                       // �롣
-	timeStamp1.tm_isdst = 0;                          // ������ɦ��
-	sscanf(info[numFlavorType + 7], "%d-%d-%d %d:%d:%d", &year, &month, &day, &hour, &minute, &second);
-	timeStamp2.tm_year = year - 1900;                 // �꣬����tm�ṹ��洢���Ǵ�1900�꿪ʼ��ɦ�䣬����tm_yearΪint��ɦ������ȥ1900��
-	timeStamp2.tm_mon = month - 1;                    // �£�����tm�ṹ����·ݴ洢��ΧΪ0-11������tm_monΪint��ɦ������ȥ1��
-	timeStamp2.tm_mday = day;                         // �ա�
-	timeStamp2.tm_hour = hour;                        // ɦ��
-	timeStamp2.tm_min = minute;                       // �֡�
-	timeStamp2.tm_sec = second;                       // �롣
-	timeStamp2.tm_isdst = 0;                          // ������ɦ��
-	time_t tS1 = mktime(&timeStamp1);
-	time_t tS2 = mktime(&timeStamp2);
-	int predictTP = difftime(tS2, tS1) / 86400;
-	//��ȡѵ������
-	struct tm timeStamp3;
-	char uid[512] = "";
-	char flavorName[10]= "";
-	sscanf(data[0], "%s\t%s\t%d-%d-%d %d:%d:%d", uid, flavorName, &year, &month, &day, &hour, &minute, &second);
-	timeStamp3.tm_year = year - 1900;                 // �꣬����tm�ṹ��洢���Ǵ�1900�꿪ʼ��ɦ�䣬����tm_yearΪint��ɦ������ȥ1900��
-	timeStamp3.tm_mon = month - 1;                    // �£�����tm�ṹ����·ݴ洢��ΧΪ0-11������tm_monΪint��ɦ������ȥ1��
-	timeStamp3.tm_mday = day;                         // �ա�
-	timeStamp3.tm_hour = 0;                        // ɦ��
-	timeStamp3.tm_min = 0;                       // �֡�
-	timeStamp3.tm_sec = 0;                       // �롣
-	timeStamp3.tm_isdst = 0;                          // ������ɦ��
-	time_t tS3 = mktime(&timeStamp3);
-	int trainTP = difftime(tS1, tS3) / 86400;
-	vector<vector<double>> trainData;
-	vector<double> tmpTrain;
+    }
+    char optType[5] = "";
+    sscanf(info[numFlavorType + 4], "%s", optType);
+    bool memOrCPU;
+    if (!strcmp(optType, "CPU"))
+        memOrCPU = true;
+    else
+        memOrCPU = false;
+    struct tm timeStamp1, timeStamp2;
+    int year, month, day, hour, minute, second;
+    sscanf(info[numFlavorType + 6], "%d-%d-%d %d:%d:%d", &year, &month, &day, &hour, &minute, &second);
+    timeStamp1.tm_year = year - 1900;                 // �꣬����tm�ṹ��洢���Ǵ�1900�꿪ʼ��ɦ�䣬����tm_yearΪint��ɦ������ȥ1900��
+    timeStamp1.tm_mon = month - 1;                    // �£�����tm�ṹ����·ݴ洢��ΧΪ0-11������tm_monΪint��ɦ������ȥ1��
+    timeStamp1.tm_mday = day;                         // �ա�
+    timeStamp1.tm_hour = hour;                        // ɦ��
+    timeStamp1.tm_min = minute;                       // �֡�
+    timeStamp1.tm_sec = second;                       // �롣
+    timeStamp1.tm_isdst = 0;                          // ������ɦ��
+    sscanf(info[numFlavorType + 7], "%d-%d-%d %d:%d:%d", &year, &month, &day, &hour, &minute, &second);
+    timeStamp2.tm_year = year - 1900;                 // �꣬����tm�ṹ��洢���Ǵ�1900�꿪ʼ��ɦ�䣬����tm_yearΪint��ɦ������ȥ1900��
+    timeStamp2.tm_mon = month - 1;                    // �£�����tm�ṹ����·ݴ洢��ΧΪ0-11������tm_monΪint��ɦ������ȥ1��
+    timeStamp2.tm_mday = day;                         // �ա�
+    timeStamp2.tm_hour = hour;                        // ɦ��
+    timeStamp2.tm_min = minute;                       // �֡�
+    timeStamp2.tm_sec = second;                       // �롣
+    timeStamp2.tm_isdst = 0;                          // ������ɦ��
+    time_t tS1 = mktime(&timeStamp1);
+    time_t tS2 = mktime(&timeStamp2);
+    int predictTP = difftime(tS2, tS1) / 86400;
+    //��ȡѵ������
+    struct tm timeStamp3;
+    char uid[512] = "";
+    char flavorName[10]= "";
+    sscanf(data[0], "%s\t%s\t%d-%d-%d %d:%d:%d", uid, flavorName, &year, &month, &day, &hour, &minute, &second);
+    timeStamp3.tm_year = year - 1900;                 // �꣬����tm�ṹ��洢���Ǵ�1900�꿪ʼ��ɦ�䣬����tm_yearΪint��ɦ������ȥ1900��
+    timeStamp3.tm_mon = month - 1;                    // �£�����tm�ṹ����·ݴ洢��ΧΪ0-11������tm_monΪint��ɦ������ȥ1��
+    timeStamp3.tm_mday = day;                         // �ա�
+    timeStamp3.tm_hour = 0;                        // ɦ��
+    timeStamp3.tm_min = 0;                       // �֡�
+    timeStamp3.tm_sec = 0;                       // �롣
+    timeStamp3.tm_isdst = 0;                          // ������ɦ��
+    time_t tS3 = mktime(&timeStamp3);
+    int trainTP = difftime(tS1, tS3) / 86400;
+    vector<vector<double>> trainData;
+    vector<double> tmpTrain;
     for (int j = 0; j < trainTP; j++) {
         tmpTrain.push_back(0);
     }
-	for (int i = 0; i < numFlavorType; i++) {
-	    trainData.push_back(tmpTrain);
-	}
-//	if (flavorName <= numFlavorType)   trainData[flavorName - 1][0] += 1;
+    for (int i = 0; i < numFlavorType; i++) {
+        trainData.push_back(tmpTrain);
+    }
+//  if (flavorName <= numFlavorType)   trainData[flavorName - 1][0] += 1;
     int flavorIndex = 0;
     bool flavorFlag = false;
     for(int i=0;i<numFlavorType;i++){
@@ -94,29 +94,29 @@ void predict_server(char * info[MAX_INFO_NUM], char * data[MAX_DATA_NUM], int da
         }
     }
     if (flavorFlag) trainData[flavorIndex][0] += 1;
-	for (int i = 1; i < data_num; i++) {
-		sscanf(data[i], "%s\t%s\t%d-%d-%d %d:%d:%d", uid, flavorName, &year, &month, &day, &hour, &minute, &second);
+    for (int i = 1; i < data_num; i++) {
+        sscanf(data[i], "%s\t%s\t%d-%d-%d %d:%d:%d", uid, flavorName, &year, &month, &day, &hour, &minute, &second);
         flavorFlag = false;
-		for(int i=0;i<numFlavorType;i++){
+        for(int i=0;i<numFlavorType;i++){
             if(!strcmp(V_name[i],flavorName)){
                 flavorIndex = i;
                 flavorFlag = true;
             }
         }
         if (!flavorFlag)    continue;
-		timeStamp3.tm_year = year - 1900;                 // �꣬����tm�ṹ��洢���Ǵ�1900�꿪ʼ��ɦ�䣬����tm_yearΪint��ɦ������ȥ1900��
-		timeStamp3.tm_mon = month - 1;                    // �£�����tm�ṹ����·ݴ洢��ΧΪ0-11������tm_monΪint��ɦ������ȥ1��
-		timeStamp3.tm_mday = day;                         // �ա�
-		timeStamp3.tm_hour = 0;                        // ɦ��
-		timeStamp3.tm_min = 0;                       // �֡�
-		timeStamp3.tm_sec = 0;                       // �롣
-		timeStamp3.tm_isdst = 0;                          // ������ɦ��
-		time_t tS3 = mktime(&timeStamp3);
-		int tmpTP = difftime(tS1, tS3) / 86400;
-		trainData[flavorIndex][trainTP-tmpTP] += 1;
-	}
+        timeStamp3.tm_year = year - 1900;                 // �꣬����tm�ṹ��洢���Ǵ�1900�꿪ʼ��ɦ�䣬����tm_yearΪint��ɦ������ȥ1900��
+        timeStamp3.tm_mon = month - 1;                    // �£�����tm�ṹ����·ݴ洢��ΧΪ0-11������tm_monΪint��ɦ������ȥ1��
+        timeStamp3.tm_mday = day;                         // �ա�
+        timeStamp3.tm_hour = 0;                        // ɦ��
+        timeStamp3.tm_min = 0;                       // �֡�
+        timeStamp3.tm_sec = 0;                       // �롣
+        timeStamp3.tm_isdst = 0;                          // ������ɦ��
+        time_t tS3 = mktime(&timeStamp3);
+        int tmpTP = difftime(tS1, tS3) / 86400;
+        trainData[flavorIndex][trainTP-tmpTP] += 1;
+    }
 
-	vector<int> V_num;
+    vector<int> V_num;
     try {
         for (int i = 0; i < numFlavorType; i++) {
             int tmpAccu = 0;
@@ -145,17 +145,17 @@ void predict_server(char * info[MAX_INFO_NUM], char * data[MAX_DATA_NUM], int da
                         tmpPredict[k] = arima->aftDeal(predictDiff, period);
                         cnt++;
                     }
-//				std::cout<<bestModel[0]<<" "<<bestModel[1]<<std::endl;
+//              std::cout<<bestModel[0]<<" "<<bestModel[1]<<std::endl;
                     list.push_back(bestModel);
                 }
 
-//			double sumPredict = 0.0;
+//          double sumPredict = 0.0;
                 int sumPredict = 0;
                 for (int k = 0; k < cnt; ++k) {
                     if (sumPredict < tmpPredict[k]) sumPredict = tmpPredict[k];
-//				sumPredict += ((double)tmpPredict[k])/(double)cnt;
+//              sumPredict += ((double)tmpPredict[k])/(double)cnt;
                 }
-//			int predict = (int)round(sumPredict);
+//          int predict = (int)round(sumPredict);
                 int predict = sumPredict;
                 tmpAccu += predict;
                 trainData[i].push_back(predict);
@@ -167,8 +167,15 @@ void predict_server(char * info[MAX_INFO_NUM], char * data[MAX_DATA_NUM], int da
     {
         for (int i=0; i<numFlavorType; i++){
             V_num[i] = 0;
-            for( int j=trainTP-predictTP; j<trainTP; j++){
-                V_num[i]+= trainData[i][j];
+            if (trainTP<predictTP) {
+                for( int j=0; j<trainTP; j++){
+                    V_num[i]+= trainData[i][j];
+                }
+                V_num[i] = V_num[i]*predictTP/trainTP;
+            }else{
+                for( int j=trainTP-predictTP; j<trainTP; j++){
+                    V_num[i]+= trainData[i][j];
+                }
             }
         }
     }
@@ -190,7 +197,13 @@ void predict_server(char * info[MAX_INFO_NUM], char * data[MAX_DATA_NUM], int da
         result = DP_Bag(MEM, CPU, V_MEM, V_CPU, V_num, memOrCPU);
     }
     catch (exception& e) {
-        result = Greedy_Bag(MEM, CPU, V_MEM, V_CPU, V_num, memOrCPU);
+        // result = Greedy_Bag(MEM, CPU, V_MEM, V_CPU, V_num, memOrCPU);
+        vector<int> one_phy;
+        for (int i =0; i<V_num.size(); i++)
+        {
+            one_phy.push_back(1);
+        }
+        result.push_back(one_phy);
     }
 
     sprintf(tmp,"%d\n", result.size());
@@ -216,6 +229,6 @@ void predict_server(char * info[MAX_INFO_NUM], char * data[MAX_DATA_NUM], int da
         strcat(result_file,tmp);
     }
 
-	// ֱ�ӵ�������ļ��ķ��������ָ���ļ���(ps��ע���ʽ����ȷ�ԣ�����н⣬��һ��ֻ��һ�����ݣ��ڶ���Ϊ�գ������п�ʼ���Ǿ�������ݣ�����֮����һ���ո�ָ���)
-	write_result(result_file, filename);
+    // ֱ�ӵ�������ļ��ķ��������ָ���ļ���(ps��ע���ʽ����ȷ�ԣ�����н⣬��һ��ֻ��һ�����ݣ��ڶ���Ϊ�գ������п�ʼ���Ǿ�������ݣ�����֮����һ���ո�ָ���)
+    write_result(result_file, filename);
 }
